581a88dc6e644a68d16bf2c25b889a73674fe2fa
Arex <zjfbob@gmail.com>
2017-08-31 21:05:56 +0800

    group chat

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 flag = 0
+flag_group = 0
 
 #prefix = "[自动回复]"
 
 textUserName = "Arex"
-groupUserName = "创新创业团队"
+groupUserName = "阿瑞斯"
 
 @itchat.msg_register('Text')
 def text_reply(msg):
@@ -21,21 +22,23 @@ def text_reply(msg):
 		return '\
 回复 open 开启机器人\n\
 回复 close 关闭机器人\n\
-回复 arex 浏览我的博客\n\
-回复 picture 查看图片'
+回复 arex 浏览我的博客'
 	elif 'open' in msg['Text'].lower(): 
 		flag = 1
-		return '开启机器人'
+		return '机器人已开启'
 	elif 'close' in msg['Text'].lower(): 
 		flag = 0
-		return '关闭机器人'
+		return '机器人已关闭'
 	elif 'arex' in msg['Text'].lower(): 
 		return 'http://arexchu.github.io'
-	elif 'picture' in msg['Text'].lower():
-		itchat.send('@img@Himalayas.jpg', msg['FromUserName']) # there should be a picture
+	#elif 'picture' in msg['Text'].lower():
+	#	itchat.send('@img@Himalayas.jpg', msg['FromUserName']) # there should be a picture
 	elif flag:
 		print '%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName']
 		if msg['FromUserName'] == friends:
+			reply = get_response(msg['Text'])
+			print reply
+		else:
 			reply = get_response(msg['Text'])
 			#reply = prefix + reply
 			print reply
@@ -45,17 +48,33 @@ def text_reply(msg):
 
 @itchat.msg_register('Text', isGroupChat = True)
 def group_reply(msg):
-	print '@%s\u2005%s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName']
+	global flag_group
 	if msg['FromUserName'] == chatrooms: 
-	#if msg['isAt']:
-		reply = get_response(msg['Content'])
-		print reply
-		return reply
+		if msg['isAt']:
+			return '\
+回复 open 开启机器人\n\
+回复 close 关闭机器人\n\
+回复 arex 浏览我的博客'
+		elif 'open' in msg['Text'].lower(): 
+			flag_group = 1
+			return '机器人已开启'
+		elif 'close' in msg['Text'].lower(): 
+			flag_group = 0
+			return '机器人已关闭'
+		elif 'arex' in msg['Text'].lower(): 
+			return 'http://arexchu.github.io'
+		elif flag_group:
+			print '%s: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName']
+			reply = get_response(msg['Content'])
+			print reply
+			return reply
+		else:
+			return
 
 itchat.auto_login(True)
-itchat.get_friends(update=True)
+#itchat.get_friends(update=True)
 friends = itchat.search_friends(name=textUserName)[0]["UserName"]
-print friends
+#print friends
 chatrooms = itchat.search_chatrooms(name=groupUserName)[0]["UserName"]
-print chatrooms
+#print chatrooms
 itchat.run()
