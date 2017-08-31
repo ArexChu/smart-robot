3ecef9de32505d81e44ce80a93de6da0ea1eb555
Arex <zjfbob@gmail.com>
2017-08-31 22:40:24 +0800

    control

--- main.py
+++ main.py
@@ -12,69 +12,100 @@ flag_group = 0
 
 #prefix = "[自动回复]"
 
-textUserName = "Arex"
-groupUserName = "阿瑞斯"
+myUserName = "Arex"
+friends = "@"
+chatrooms = "@@"
 
 @itchat.msg_register('Text')
 def text_reply(msg):
 	global flag
-	if 'help' in msg['Text'].lower():
-		return '\
-回复 open 开启机器人\n\
-回复 close 关闭机器人\n\
+	if msg['FromUserName'] == arex:
+		global flag_group
+		if 'help' in msg['Text'].lower():
+			return '\
+input set friend or chatroom to choose a object\n\
+input enable friend or chatroom to open the robot\n\
+input disable friend or chatroom to close the robot\n\
+input arex to visit my blog'
+		if 'set' in msg['Text'].lower(): 
+			if 'friend' in msg['Text'].lower(): 
+				global friends
+				textUserName = msg['Text']
+				otherUserName = textUserName.split(" ")[2]
+				friends = itchat.search_friends(name=otherUserName)[0]["UserName"]
+				#print friends
+			if 'chatroom' in msg['Text'].lower(): 
+				global chatrooms
+				contentUserName = msg['Text']
+				groupUserName = contentUserName.split(" ")[2]
+				chatrooms = itchat.search_chatrooms(name=groupUserName)[0]["UserName"]
+				#print chatrooms
+			#reply = get_response(msg['Text'])
+			#print reply
+			return 'object enabled'
+		if 'enable' in msg['Text'].lower(): 
+			if 'friend' in msg['Text'].lower(): 
+				flag = 1
+				return 'robot enabled'
+			if 'chatroom' in msg['Text'].lower(): 
+				flag_group = 1
+				return 'robot enabled'
+		if 'disable' in msg['Text'].lower(): 
+			if 'friend' in msg['Text'].lower(): 
+				flag = 0
+				return 'robot disabled'
+			if 'chatroom' in msg['Text'].lower(): 
+				flag_group = 0
+				return 'robot disabled'
+	if msg['FromUserName'] == friends:
+		if 'help' in msg['Text'].lower():
+			return '\
+回复 enable 开启arex机器人\n\
+回复 disable 关闭arex机器人\n\
 回复 arex 浏览我的博客'
-	elif 'open' in msg['Text'].lower(): 
-		flag = 1
-		return '机器人已开启'
-	elif 'close' in msg['Text'].lower(): 
-		flag = 0
-		return '机器人已关闭'
-	elif 'arex' in msg['Text'].lower(): 
-		return 'http://arexchu.github.io'
-	#elif 'picture' in msg['Text'].lower():
-	#	itchat.send('@img@Himalayas.jpg', msg['FromUserName']) # there should be a picture
-	elif flag:
-		print '%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName']
-		if msg['FromUserName'] == friends:
-			reply = get_response(msg['Text'])
-			print reply
-		else:
+		if 'enable' in msg['Text'].lower(): 
+			flag = 1
+			return 'robot enabled'
+		if 'disable' in msg['Text'].lower(): 
+			flag = 0
+			return 'robot disabled'
+		if 'arex' in msg['Text'].lower(): 
+			return 'http://arexchu.github.io'
+		#elif 'picture' in msg['Text'].lower():
+		#	itchat.send('@img@Himalayas.jpg', msg['FromUserName']) # there should be a picture
+		if flag:
+			print '%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName']
 			reply = get_response(msg['Text'])
 			#reply = prefix + reply
 			print reply
 			return reply
-	else:
-		return
 
 @itchat.msg_register('Text', isGroupChat = True)
 def group_reply(msg):
-	global flag_group
 	if msg['FromUserName'] == chatrooms: 
 		if msg['isAt']:
-			return '\
-回复 open 开启机器人\n\
-回复 close 关闭机器人\n\
-回复 arex 浏览我的博客'
-		elif 'open' in msg['Text'].lower(): 
-			flag_group = 1
-			return '机器人已开启'
-		elif 'close' in msg['Text'].lower(): 
-			flag_group = 0
-			return '机器人已关闭'
-		elif 'arex' in msg['Text'].lower(): 
-			return 'http://arexchu.github.io'
-		elif flag_group:
+			global flag_group
+			if 'help' in msg['Text'].lower():
+				return '\
+回复 enable 同时@我 开启arex机器人\n\
+回复 disable 同时@我 关闭arex机器人\n\
+回复 arex 同时@我浏览我的博客'
+			if 'enable' in msg['Text'].lower(): 
+				flag_group = 1
+				return 'robot enabled'
+			if 'disable' in msg['Text'].lower(): 
+				flag_group = 0
+				return 'robot disabled'
+			if 'arex' in msg['Text'].lower(): 
+				return 'http://arexchu.github.io'
+		if flag_group:
 			print '%s: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName']
 			reply = get_response(msg['Content'])
 			print reply
 			return reply
-		else:
-			return
 
 itchat.auto_login(True)
-#itchat.get_friends(update=True)
-friends = itchat.search_friends(name=textUserName)[0]["UserName"]
-#print friends
-chatrooms = itchat.search_chatrooms(name=groupUserName)[0]["UserName"]
-#print chatrooms
+itchat.get_friends(update=True)
+arex = itchat.search_friends(name=myUserName)[0]["UserName"]
+#print arex
 itchat.run()
